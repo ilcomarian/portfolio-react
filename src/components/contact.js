2f95ffd52ad52cd632e9ac64380d0d62aa7283e0
ilcomarian <ilcomarian@gmail.com>
2018-11-03 21:09:46 -0400

    style contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
-
+import { Grid, Cell } from "react-mdl";
 class Contact extends Component {
   state = {};
   render() {
     return (
-      <div classNam="contact-page">
-        <div className="contact">
-          <img src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" />
-        </div>
-        <div className="contact">
-          <h1>Contact me</h1>
-        </div>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Marian Ilco</h2>
+            <img src="https://www.w3schools.com/howto/img_avatar.png" />
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+          </Cell>
+        </Grid>
       </div>
     );
   }
